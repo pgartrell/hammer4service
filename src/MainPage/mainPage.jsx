52a5005816cfa20fa25aca6ded5ccd36e04303fd
Precious Gartrell <precious.gartrell@allovue.com>
2024-02-06 23:56:38 -0500

    navbar and different background pic

---
+++ src/MainPage/mainPage.jsx
@@ -0,0 +1,14 @@
+import '../MainPage/mainPageCss.css'
+
+function MainPage() {
+
+  return (
+    <>
+      <div className='mainDiv'>
+        Testing
+      </div>
+    </>
+  )
+}
+
+export default MainPage
