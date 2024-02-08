b6d72b74d7a723a527f2ae66df6e7f069d314317
Precious Gartrell <precious.gartrell@allovue.com>
2024-02-08 08:25:07 -0500

    case sensitive mainpage folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
 import NavBar from './navbar/navbar.jsx';
+import MainPage from './mainpage/mainpage.jsx';
 
 
 function App() {
   return (
     <>
     <NavBar />
- 
+    <MainPage />
     </>
   )
 }
