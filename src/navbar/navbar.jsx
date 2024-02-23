651f053c4a5c0363ec869298ca68a58db4116482
Precious Gartrell <precious.gartrell@allovue.com>
2024-02-22 23:23:10 -0500

    redoing navbar

--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -2,39 +2,81 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './navbarcss.css';
+// import { UisBars } from '@iconscout/react-unicons'
 
-function NavBar() {
-  return (
-    <>
-      <Navbar collapseOnSelect expand="md" className="navbar bg-body-tertiary" bg="dark" data-bs-theme="dark"> 
-        <Container>
-        <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src="../src/assets/logo2.png"
-              width="100"
-              height="50"
-              className="logo--image d-inline-block align-top"
-            />
-          </Navbar.Brand>
-            <Navbar.Brand 
-              href="#home" 
-              className='logo--title'>
-                  Hammer4Service, LLC
-            </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">        
+// import { useState } from 'react';
+
+
+
+  export default function NavBar() {
+    // const Navbar = () => {
+    //   const [showMenu, setShowMenu] = useState(false);
+     
+    //   const toggleMenu = () => {
+    //     setShowMenu(!showMenu);
+    //   };
+     
+    //    const closeMenuOnMobile = () => {
+    //      if (window.innerWidth <= 1150) {
+    //        setShowMenu(false);
+    //      }
+    //    };
+
+     return (
+      <header className='navbar'>
+        <nav className='nav container'>
+          <a href='#home'>
+           <img
+           alt=""
+           src="../src/assets/logo2.png"
+           width="100"
+           height="50"
+           className="logo--image d-inline-block align-top"
+         />
+          </a>
+          <a href="#home" className='logo--title'>
+                Hammer4Service, LLC
+          </a>
+
+          <div
+        // className={`nav__menu ${showMenu ? "show-menu" : ""}`}
+          id="nav-menu"
+          >  
             <Nav className="ms-auto">
-              <Nav.Link className="navbar--links" href="#home">Home</Nav.Link>
-              <Nav.Link className="navbar--links"href="#features">Gallery</Nav.Link>
-              <Nav.Link className="navbar--links"href="#pricing">Get a Quote</Nav.Link>
+              <a className="navbar--links" href="#home">Home</a>
+              <a className="navbar--links"href="#gallery">Gallery</a>
+              <a className="navbar--links"href="#quote">Get a Quote</a>
             </Nav>
-          </Navbar.Collapse>
-        </Container>  
-      </Navbar>  
+            {/* <Unicons.UisBars size="140" color="#61DAFB" /> */}
+          </div>
+        </nav>
+      </header>
+     )
+  }
 
-    </>
-  )
-}
-
-export default NavBar
+//      <Nav collapseOnSelect expand="md" className="navbar bg-body-tertiary" bg="dark" data-bs-theme="dark"> 
+//      <Container>
+//      <Navbar.Brand href="#home">
+//          <img
+//            alt=""
+//            src="../src/assets/logo2.png"
+//            width="100"
+//            height="50"
+//            className="logo--image d-inline-block align-top"
+//          />
+//        </Navbar.Brand>
+//          <Navbar.Brand 
+//            href="#home" 
+//            className='logo--title'>
+//                Hammer4Service, LLC
+//          </Navbar.Brand>
+//        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+//        <Navbar.Collapse id="responsive-navbar-nav">        
+//          <Nav className="ms-auto">
+//            <Nav.Link className="navbar--links" href="#home">Home</Nav.Link>
+//            <Nav.Link className="navbar--links"href="#features">Gallery</Nav.Link>
+//            <Nav.Link className="navbar--links"href="#pricing">Get a Quote</Nav.Link>
+//          </Nav>
+//        </Navbar.Collapse>
+//      </Container>  
+//  </Nav>  
